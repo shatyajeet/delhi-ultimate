6f2643cb1852a83e1fffe10be8c00d58f01406fd
shatyajeet@gmail.com <satyajeetparida@Satyajeets-MacBook-Pro.local>
2018-06-13 14:14:23 +0530

    Updated code

--- src/js/App.js
+++ src/js/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import {Router, browserHistory} from 'react-router';
-
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import routes from './routes';
-
-injectTapEventPlugin();
 
 export default function App () {
   return (
